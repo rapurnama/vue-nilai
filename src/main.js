c17b4859c139551ef527319374b8623759cd7238
Ababil Mustaqim <95958484+birdfromhell@users.noreply.github.com>
2024-10-24 08:17:13 +0000

    Refactor file structure and remove unused files

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './routers'
-createApp(App).use(router).mount('#app')
+import app from './config/configApp';
+import router from './routes/protectedRoute';
+import store from '@/vuex/store';
+import './static/css/style.css';
+// Vue 3rd party plugins
+import '@/core/plugins/ant-design';
+import '@/core/plugins/fonts';
+import '@/core/plugins/unicons';
+import '@/core/components/custom';
+import '@/core/components/style';
+import '@/i18n/config';
+
+app.config.productionTip = false;
+app.use(store);
+app.use(router);
+app.mount('#app');
